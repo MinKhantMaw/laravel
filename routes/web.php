5de749481f42c969d9f16358f590e2e458dfc9a5
MinKhantMaw <mr.minkhantmaw@gmail.com>
2021-11-02 14:34:36 +0630

    week4

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/',[HomeController::class,'index']);
+// Route::get('/',[HomeController::class,'index']);
+
+Route::resource('/', HomeController::class);
